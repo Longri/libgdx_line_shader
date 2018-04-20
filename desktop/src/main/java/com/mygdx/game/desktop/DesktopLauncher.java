54b5656a6a88a5c0ff5ba039a840cbc509323222
Longri <www.longri.de@googlemail.com>
2018-04-20 14:04:01 +0200

    ?

--- desktop/src/main/java/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,25 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglFileHandle;
 import com.mygdx.game.MyGdxGame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.LibgdxLogger;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LibgdxLogger.PROPERTIES_FILE_HANDLE = new LwjglFileHandle(LibgdxLogger.CONFIGURATION_FILE_XML, Files.FileType.Local);
+
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        LwjglApplication application = new LwjglApplication(new MyGdxGame(), config);
+
+        application.setLogLevel(LwjglApplication.LOG_DEBUG);
+
+        Logger log=LoggerFactory.getLogger("a");// initial LoggerFactory after application creation!
+
+    }
 }
